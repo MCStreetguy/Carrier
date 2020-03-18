193c90ef5ddf5756cf0adf1e946cdaa466350d2f
Maximilian Schmidt <m.schmidt@obis-concept.de>
2020-03-18 20:58:57 +0100

    FIX: fix permissions script order

--- container/etc/carrier/init/00-fix-permissions.sh
+++ container/etc/carrier/init/00-fix-permissions.sh
@@ -1,9 +1,9 @@
 #!/bin/execlineb -S0
 
 if { s6-echo "[carrier-init] fixing general filesystem permissions..." }
-if { elglob FILES /bin/* chmod 0755 ${FILES} }
-if { elglob FILES /usr/bin/* chmod 0755 ${FILES} }
 if { elglob FILES /sbin/* chmod 0740 ${FILES} }
 if { elglob FILES /usr/sbin/* chmod 0740 ${FILES} }
+if { elglob FILES /bin/* chmod 0755 ${FILES} }
+if { elglob FILES /usr/bin/* chmod 0755 ${FILES} }
 
 exit 0
